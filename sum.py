4f80a48b69af4c4e2803ca5ca32b22c3a5045ee2
shkstbma14 <shk.stbma14@gmail.com>
2025-05-01 20:23:37 +0530

    modify varriables num1,2,3,4

--- sum.py
+++ sum.py
@@ -1,7 +1,7 @@
 def addition(num1, num2):
     numbers = [num1, num2]
-    return sum(numbers)
+    return sum(num1+num2)
 
-def substraction(num1, num2):
-    numbers = [num1, num2]
-    return (num1 - num2)
+def substraction(num3, num4):
+    numbers = [num3, num4]
+    return (num3 - num4)
